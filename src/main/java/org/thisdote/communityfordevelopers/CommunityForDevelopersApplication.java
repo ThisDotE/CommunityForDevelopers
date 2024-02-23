683a57d06d90076d513a63e365ccfb63fbd858b9
jhlee6515 <45275759+jhlee6515@users.noreply.github.com>
2024-02-23 14:49:15 +0900

    init project

---
+++ src/main/java/org/thisdote/communityfordevelopers/CommunityForDevelopersApplication.java
@@ -0,0 +1,13 @@
+package org.thisdote.communityfordevelopers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityForDevelopersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityForDevelopersApplication.class, args);
+    }
+
+}
