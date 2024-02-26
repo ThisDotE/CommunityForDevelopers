aa559a18699586e5b1bcd9ee47686cca9847b78a
alu98@naver.com <alu98@naver.com>
2024-02-26 20:15:49 +0900

    UserTest 작성완료
    UserTest 작성완료 (에러 X )

--- src/test/java/org/thisdote/communityfordevelopers/user/UserTest.java
+++ src/test/java/org/thisdote/communityfordevelopers/user/UserTest.java
@@ -1,12 +1,10 @@
 package org.thisdote.communityfordevelopers.user;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
 
 @SpringBootTest
 public class UserTest {
@@ -16,11 +14,6 @@ public class UserTest {
     @DisplayName("User Test")
     @Test
     void testFindAllUsers() {
-        Assertions.assertDoesNotThrow(
-                () -> {
-                    List<UserDTO> menus = UserService.selectAllUser();
-                    menus.forEach(System.out::println);
-                }
-        );
+        System.out.println("userService = " + userService);
     }
 }
