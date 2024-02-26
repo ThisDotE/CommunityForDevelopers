0e3e016c397a6be23dd13ed86050eb94624db492
shindaeyeong <eodud3196@gmail.com>
2024-02-26 18:56:30 +0900

    Test01 디렉토리
    디렉토리 삭제 후 하위 디렉토리 상위 디렉토리로 이동.

--- src/test/java/org/thisdote/communityfordevelopers/ReportedArticleTest.java
+++ src/test/java/org/thisdote/communityfordevelopers/ReportedArticleTest.java
@@ -1,4 +1,4 @@
-package org.thisdote.communityfordevelopers.test01.reported_article;
+package org.thisdote.communityfordevelopers;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
--- src/test/java/org/thisdote/communityfordevelopers/reported_article/CommunityForDevelopersApplicationTests.java
+++ src/test/java/org/thisdote/communityfordevelopers/reported_article/CommunityForDevelopersApplicationTests.java
@@ -1,4 +1,4 @@
-package org.thisdote.communityfordevelopers;
+package org.thisdote.communityfordevelopers.reported_article;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
