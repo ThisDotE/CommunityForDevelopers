f2595576936bc433745fd5b60bbd5b3e4fa22781
shindaeyeong <eodud3196@gmail.com>
2024-02-26 19:09:44 +0900

    ReportedArticle 오류
    ReportedArticle Mapper 어노테이션 추가로 오류 해결

--- src/main/java/org/thisdote/communityfordevelopers/reported_article/ReportedArticleMapper.java
+++ src/main/java/org/thisdote/communityfordevelopers/reported_article/ReportedArticleMapper.java
@@ -1,7 +1,10 @@
 package org.thisdote.communityfordevelopers.reported_article;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import java.util.List;
 
+@Mapper
 public interface ReportedArticleMapper {
 
     List<ReportedArticleDTO> selectAllReportedArticle();
