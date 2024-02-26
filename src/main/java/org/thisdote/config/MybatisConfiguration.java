fcf913bb7c46c29b04b0225e75ab5397f808d0fe
shindaeyeong <eodud3196@gmail.com>
2024-02-26 18:43:34 +0900

    MybatisConfiguration 작성
    MapperScan 사용해서 Configuration 작성

--- src/main/java/org/thisdote/config/MybatisConfiguration.java
+++ src/main/java/org/thisdote/config/MybatisConfiguration.java
@@ -1,6 +1,10 @@
 package org.thisdote.config;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan(basePackages = "org.thisdote.communityfordevelopers.test01", annotationClass = Mapper.class)
 public class MybatisConfiguration {
-
-
 }
