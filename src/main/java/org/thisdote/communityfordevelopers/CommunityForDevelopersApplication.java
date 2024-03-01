86829785595cf6b216f37ecf470b3d3bf3dd4bae
alu98@naver.com <alu98@naver.com>
2024-03-01 12:41:47 +0900

    ReportedReplyTest 메소드명 수정

--- src/main/java/org/thisdote/communityfordevelopers/CommunityForDevelopersApplication.java
+++ src/main/java/org/thisdote/communityfordevelopers/CommunityForDevelopersApplication.java
@@ -8,6 +8,8 @@ public class CommunityForDevelopersApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CommunityForDevelopersApplication.class, args);
+
+        System.out.println("신동호 feature/question-article branch 잘 파졌는지 확인");
     }
 
 }
