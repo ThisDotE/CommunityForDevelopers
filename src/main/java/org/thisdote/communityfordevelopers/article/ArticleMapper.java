afe9255ad0c37b53e42d1a7511c4205abf876f2b
jhlee6515 <45275759+jhlee6515@users.noreply.github.com>
2024-03-02 11:59:33 +0900

    [feature] 회원 별 게시글 조회 쿼리 작성 및 테스트 코드 작성

--- src/main/java/org/thisdote/communityfordevelopers/article/ArticleMapper.java
+++ src/main/java/org/thisdote/communityfordevelopers/article/ArticleMapper.java
@@ -7,4 +7,6 @@ import java.util.List;
 @Mapper
 public interface ArticleMapper {
     List<ArticleDTO> selectAllArticle();
+
+    List<ArticleDTO> selectArticleByUser(int userCode);
 }
--- src/test/java/org/thisdote/communityfordevelopers/article/ArticleTest.java
+++ src/test/java/org/thisdote/communityfordevelopers/article/ArticleTest.java
@@ -3,8 +3,12 @@ package org.thisdote.communityfordevelopers.article;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.thisdote.communityfordevelopers.inquiry.InquiryDTO;
+import org.thisdote.communityfordevelopers.user.UserDTO;
 
 import java.util.List;
 
@@ -23,5 +27,15 @@ public class ArticleTest {
                     articles.forEach(System.out::println);
                 }
         );
+    }
+
+    @DisplayName("회원 별 작성 게시글 조회")
+    @ParameterizedTest
+    @ValueSource(ints = 1)
+    void testFindArticleByUser(int userCode) {
+        Assertions.assertDoesNotThrow(() -> {
+            List<ArticleDTO> articleByUser = articleService.selectArticleByUser(userCode);
+            articleByUser.forEach(System.out::println);
+        });
     }
 }
