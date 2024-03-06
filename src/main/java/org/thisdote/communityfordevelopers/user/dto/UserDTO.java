2cdc753c90a22671e57cda54cfc3f6df53fdee85
jhlee6515 <45275759+jhlee6515@users.noreply.github.com>
2024-03-06 17:05:11 +0900

    UserDTO constructor, getter/setter, toString 수정

--- src/main/java/org/thisdote/communityfordevelopers/user/dto/UserDTO.java
+++ src/main/java/org/thisdote/communityfordevelopers/user/dto/UserDTO.java
@@ -21,20 +21,20 @@ public class UserDTO {
     public UserDTO() {
     }
 
-    public UserDTO(int userCode, String userId, String userPassword, Date userBirthday, String userPhone, String userEmail, int userStudygroupStatus, Date userRegistDate, Date userInfoUpdateDate, int userGrade) {
-        this.userCode = userCode;
-        this.userId = userId;
-        this.userPassword = userPassword;
-        this.userBirthday = userBirthday;
-        this.userPhone = userPhone;
-        this.userEmail = userEmail;
-        this.userStudyGroupStatus = userStudygroupStatus;
-        this.userRegistDate = userRegistDate;
-        this.userInfoUpdateDate = userInfoUpdateDate;
-        this.userGrade = userGrade;
-    }
-
-    public UserDTO(int userCode, String userId, String userPassword, Date userBirthday, String userPhone, String userEmail, int userStudyGroupStatus, Date userRegistDate, Date userInfoUpdateDate, int userGrade, StudyGroupMemberDTO studyGroupMemberDTO) {
+    public UserDTO(
+            int userCode,
+            String userId,
+            String userPassword,
+            Date userBirthday,
+            String userPhone,
+            String userEmail,
+            int userStudyGroupStatus,
+            Date userRegistDate,
+            Date userInfoUpdateDate,
+            int userGrade,
+            int userResignStatus,
+            StudyGroupMemberDTO studyGroupMemberDTO
+    ) {
         this.userCode = userCode;
         this.userId = userId;
         this.userPassword = userPassword;
@@ -45,86 +45,104 @@ public class UserDTO {
         this.userRegistDate = userRegistDate;
         this.userInfoUpdateDate = userInfoUpdateDate;
         this.userGrade = userGrade;
+        this.userResignStatus = userResignStatus;
         this.studyGroupMemberDTO = studyGroupMemberDTO;
     }
 
     public int getUserCode() {
         return userCode;
     }
-    public String getUserId() {
-        return userId;
-    }
-    public String getUserPassword() {
-        return userPassword;
-    }
-    public Date getUserBirthday() {
-        return userBirthday;
-    }
-    public String getUserPhone() {
-        return userPhone;
-    }
-    public String getUserEmail() {
-        return userEmail;
-    }
-    public int getUserStudyGroupStatus() {
-        return userStudyGroupStatus;
-    }
-    public Date getUserRegistDate() {
-        return userRegistDate;
-    }
-    public Date getUserInfoUpdateDate() {
-        return userInfoUpdateDate;
-    }
-    public int getUserGrade() {
-        return userGrade;
-    }
 
     public void setUserCode(int userCode) {
         this.userCode = userCode;
+    }
+
+    public String getUserId() {
+        return userId;
     }
 
     public void setUserId(String userId) {
         this.userId = userId;
     }
 
+    public String getUserPassword() {
+        return userPassword;
+    }
+
     public void setUserPassword(String userPassword) {
         this.userPassword = userPassword;
+    }
+
+    public Date getUserBirthday() {
+        return userBirthday;
     }
 
     public void setUserBirthday(Date userBirthday) {
         this.userBirthday = userBirthday;
     }
 
+    public String getUserPhone() {
+        return userPhone;
+    }
+
     public void setUserPhone(String userPhone) {
         this.userPhone = userPhone;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
     }
 
     public void setUserEmail(String userEmail) {
         this.userEmail = userEmail;
     }
 
+    public int getUserStudyGroupStatus() {
+        return userStudyGroupStatus;
+    }
+
     public void setUserStudyGroupStatus(int userStudyGroupStatus) {
         this.userStudyGroupStatus = userStudyGroupStatus;
+    }
+
+    public Date getUserRegistDate() {
+        return userRegistDate;
     }
 
     public void setUserRegistDate(Date userRegistDate) {
         this.userRegistDate = userRegistDate;
     }
 
+    public Date getUserInfoUpdateDate() {
+        return userInfoUpdateDate;
+    }
+
     public void setUserInfoUpdateDate(Date userInfoUpdateDate) {
         this.userInfoUpdateDate = userInfoUpdateDate;
+    }
+
+    public int getUserGrade() {
+        return userGrade;
     }
 
     public void setUserGrade(int userGrade) {
         this.userGrade = userGrade;
     }
 
-    public void setStudygroupMemberDTO(StudyGroupMemberDTO studyGroupMemberDTO) {
-        this.studyGroupMemberDTO = studyGroupMemberDTO;
+    public int getUserResignStatus() {
+        return userResignStatus;
+    }
+
+    public void setUserResignStatus(int userResignStatus) {
+        this.userResignStatus = userResignStatus;
     }
 
     public StudyGroupMemberDTO getStudyGroupMemberDTO() {
         return studyGroupMemberDTO;
+    }
+
+    public void setStudyGroupMemberDTO(StudyGroupMemberDTO studyGroupMemberDTO) {
+        this.studyGroupMemberDTO = studyGroupMemberDTO;
     }
 
     @Override
@@ -140,6 +158,7 @@ public class UserDTO {
                 ", userRegistDate=" + userRegistDate +
                 ", userInfoUpdateDate=" + userInfoUpdateDate +
                 ", userGrade=" + userGrade +
+                ", userResignStatus=" + userResignStatus +
                 ", studyGroupMemberDTO=" + studyGroupMemberDTO +
                 '}';
     }
