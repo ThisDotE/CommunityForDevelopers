fe07b540dfce1d46f506a96cb5a0cfe34d12bbb7
shindaeyeong <eodud3196@gmail.com>
2024-03-06 16:57:32 +0900

    Article 수정
    게시글 수정 테스트 완료.

--- src/test/java/org/thisdote/communityfordevelopers/article/command/ArticleTest.java
+++ src/test/java/org/thisdote/communityfordevelopers/article/command/ArticleTest.java
@@ -23,7 +23,8 @@ public class ArticleTest {
     public void 새로운_게시글_생성(){
 
         ArticleDTO article = new ArticleDTO();
-        article.setArticleTitle("글제목16");
+        article.setArticleId(26);
+        article.setArticleTitle("글6");
         article.setArticleContent("글내용16");
         article.setArticleCategory(3);
         article.setArticleCreateDate("2024-01-15 00:00:00");
@@ -40,6 +41,7 @@ public class ArticleTest {
         article.setStudygroupCurrentMemberCount(17);
         article.setStudygroupPendingMemberCount(6);
         article.setArticleDeleteStatus(1);
+
         articleService1.registArticle(article);
     }
 
@@ -51,7 +53,33 @@ public class ArticleTest {
         ArticleDTO articleDTO = new ArticleDTO();
         articleDTO.setArticleDeleteStatus(0);
 
-        articleService1.modifyArticle(articleDTO, articleId);
+        articleService1.deleteArticle(articleDTO, articleId);
+    }
+
+    @Test
+    public void 게시글_수정(){
+
+        ArticleDTO article = new ArticleDTO();
+        article.setArticleId(26);
+        article.setArticleTitle("글6");
+        article.setArticleContent("글내용16");
+        article.setArticleCategory(3);
+        article.setArticleCreateDate("2024-01-15 00:00:00");
+        article.setArticleLastUpdateDate("2024-02-01 00:00:00");
+        article.setArticleViewCount(29);
+        article.setArticleLikeCount(6);
+        article.setArticleReplyCount(0);
+        article.setArticleReportStatus(6);
+        article.setStudygroupMemberMaxCount(6);
+        article.setStudygroupRecruitmentDeadline("2024-02-11 00:00:00");
+        article.setArticleQuestionCategory("6");
+        article.setUserCode(11);
+        article.setStudygroupId(5);
+        article.setStudygroupCurrentMemberCount(17);
+        article.setStudygroupPendingMemberCount(6);
+        article.setArticleDeleteStatus(1);
+
+        articleService1.modifyArticle(article);
     }
 
 
